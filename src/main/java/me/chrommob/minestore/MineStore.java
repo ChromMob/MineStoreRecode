1e42e5ebab6ff3df75cd88e5a8987f426ca7a84d
ChromMob <filip.chromy04@gmail.com>
2023-02-02 19:10:42 +0100

    Initial commit

---
+++ src/main/java/me/chrommob/minestore/MineStore.java
@@ -0,0 +1,17 @@
+package me.chrommob.minestore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MineStore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
