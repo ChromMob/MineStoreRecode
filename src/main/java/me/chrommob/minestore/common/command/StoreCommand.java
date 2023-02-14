f7281f04b0b8e5944c300c75185cd0ae3db732ea
ChromMob <filip.chromy04@gmail.com>
2023-02-14 19:21:09 +0100

    Ignore unused StoreCommand

--- src/main/java/me/chrommob/minestore/common/command/StoreCommand.java
+++ src/main/java/me/chrommob/minestore/common/command/StoreCommand.java
@@ -11,6 +11,7 @@ import net.kyori.adventure.text.Component;
 @CommandAlias("store")
 public class StoreCommand extends BaseCommand {
     @Default
+    @SuppressWarnings("unused")
     public void onStore(final AbstractUser abstractUser) {
         final Component message = (MineStoreCommon.getInstance().miniMessage()).deserialize(((String)MineStoreCommon.getInstance().configReader().get(ConfigKey.STORE_COMMAND_MESSAGE)).replaceAll("%store_url%", (String)MineStoreCommon.getInstance().configReader().get(ConfigKey.STORE_URL)));
         abstractUser.user().sendMessage(message);
