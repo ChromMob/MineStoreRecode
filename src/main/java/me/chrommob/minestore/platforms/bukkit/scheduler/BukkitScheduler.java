9d63c9bc14a14a9e8c9fcc1a032b36afcdc6c4e4
ChromMob <filip.chromy04@gmail.com>
2023-04-07 22:45:00 +0200

    Use MethodHandler instead of Reflection

--- src/main/java/me/chrommob/minestore/platforms/bukkit/scheduler/BukkitScheduler.java
+++ src/main/java/me/chrommob/minestore/platforms/bukkit/scheduler/BukkitScheduler.java
@@ -5,12 +5,22 @@ import me.chrommob.minestore.platforms.bukkit.MineStoreBukkit;
 import org.bukkit.Bukkit;
 import org.bukkit.Server;
 
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
 import java.lang.reflect.Method;
 
 public class BukkitScheduler implements CommonScheduler {
     private MineStoreBukkit plugin;
-    private final boolean isFolia;
+    private static final boolean isFolia;
+    private static final MethodHandle execute;
+    private static final Object globalRegionScheduler;
     public BukkitScheduler(MineStoreBukkit plugin) {
+        this.plugin = plugin;
+    }
+
+    static {
+        MethodHandle execute1 = null;
+        Object globalRegionScheduler1 = null;
         boolean isFolia1;
         try {
             Class.forName("io.papermc.paper.threadedregions.RegionizedServer");
@@ -19,28 +29,43 @@ public class BukkitScheduler implements CommonScheduler {
             isFolia1 = false;
         }
         isFolia = isFolia1;
-        this.plugin = plugin;
+        if (isFolia) {
+            Class<? extends Server> serverClass = Bukkit.getServer().getClass();
+            Object globalRegionScheduler = null;
+            try {
+                for (Method method : serverClass.getMethods()) {
+                    if (method.getName().equals("getGlobalRegionScheduler")) {
+                        globalRegionScheduler = method.invoke(Bukkit.getServer());
+                        break;
+                    }
+                }
+            } catch (Throwable e) {
+                e.printStackTrace();
+            }
+            globalRegionScheduler1 = globalRegionScheduler;
+            try {
+                MethodHandles.Lookup lookup = MethodHandles.lookup();
+                for (Method method : globalRegionScheduler.getClass().getMethods()) {
+                    if (method.getName().equals("execute")) {
+                        execute1 = lookup.unreflect(method);
+                        break;
+                    }
+                }
+            } catch (Throwable e) {
+                e.printStackTrace();
+            }
+        }
+        globalRegionScheduler = globalRegionScheduler1;
+        execute = execute1;
     }
 
     @Override
     public void run(Runnable runnable) {
         if (isFolia) {
-            Server server = Bukkit.getServer();
-            for (Method method: server.getClass().getMethods()) {
-                if (method.getName().equals("getGlobalRegionScheduler")) {
-                    try {
-                        Object regionScheduler = method.invoke(server);
-                        for (Method schedulerMethods : regionScheduler.getClass().getMethods()) {
-                            if (schedulerMethods.getName().equals("execute")) {
-                                schedulerMethods.invoke(regionScheduler, plugin, runnable);
-                                break;
-                            }
-                        }
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                    break;
-                }
+            try {
+                execute.invoke(globalRegionScheduler, plugin, runnable);
+            } catch (Throwable e) {
+                e.printStackTrace();
             }
         } else {
             Bukkit.getScheduler().runTask(plugin, runnable);
