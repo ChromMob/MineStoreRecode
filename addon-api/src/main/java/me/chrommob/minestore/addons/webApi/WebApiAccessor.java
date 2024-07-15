8f6787da82dcbd8bfc182a8e0f50a3cb6f01be2a
ChromMob <filip.chromy04@gmail.com>
2024-07-15 22:23:47 +0200

    Remove currently unimplementable features from the codebase.

--- addon-api/src/main/java/me/chrommob/minestore/addons/webApi/WebApiAccessor.java
+++ addon-api/src/main/java/me/chrommob/minestore/addons/webApi/WebApiAccessor.java
@@ -1,18 +1,4 @@
 package me.chrommob.minestore.addons.webApi;
 
-import me.chrommob.minestore.addons.events.types.MineStorePurchaseEvent;
-
 public class WebApiAccessor {
-    public enum CommandReason {
-        PURCHASE,
-        SUBSCRIPTION_START,
-        SUBSCRIPTION_END
-    }
-    public static CommandReason getCommandReason(String id, String username) {
-        return CommandReason.PURCHASE;
-    }
-
-    public static CommandReason getCommandReason(MineStorePurchaseEvent event) {
-        return getCommandReason(String.valueOf(event.id()), event.username());
-    }
 }
