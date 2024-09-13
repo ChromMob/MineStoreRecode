ec57b4aa72b7f33c7b634987283ab02fb0b95c30
ChromMob <filip.chromy04@gmail.com>
2024-09-13 08:46:50 +0200

    Fix autosetup on brigadier platforms

--- common/src/main/java/me/chrommob/minestore/common/command/AutoSetupCommand.java
+++ common/src/main/java/me/chrommob/minestore/common/command/AutoSetupCommand.java
@@ -4,6 +4,7 @@ import me.chrommob.minestore.common.MineStoreCommon;
 import me.chrommob.minestore.common.config.ConfigKey;
 import me.chrommob.minestore.common.interfaces.user.AbstractUser;
 import me.chrommob.minestore.common.interfaces.user.CommonUser;
+import org.incendo.cloud.annotation.specifier.Quoted;
 import org.incendo.cloud.annotations.Argument;
 import org.incendo.cloud.annotations.Command;
 import org.incendo.cloud.annotations.Permission;
@@ -16,7 +17,7 @@ public class AutoSetupCommand {
     }
     @Permission("minestore.autosetup")
     @Command("minestore|ms autosetup <storeUrl> <apiKey> <secretKey>")
-    public void onAutoSetup(AbstractUser abstractUser, @Argument("storeUrl") String storeUrl, @Argument("apiKey") String apiKey, @Argument("secretKey") String secretKey) {
+    public void onAutoSetup(AbstractUser abstractUser, @Argument("storeUrl") @Quoted String storeUrl, @Argument("apiKey") String apiKey, @Argument("secretKey") String secretKey) {
         CommonUser user = abstractUser.user();
         user.sendMessage("Auto setup started!");
         user.sendMessage("Store URL: " + storeUrl);
