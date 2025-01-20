3022c0edafc4de1cbb535acc2aa2bd343b6f5df3
ChromMob <filip.chromy04@gmail.com>
2025-01-20 16:23:56 +0100

    Remove useless Class

--- api/src/main/java/me/chrommob/minestore/api/generic/OnlySupportedSince.java
+++
@@ -1,7 +0,0 @@
-package me.chrommob.minestore.api.generic;
-
-public class OnlySupportedSince extends UnsupportedOperationException {
-    public OnlySupportedSince(MineStoreVersion currentVersion, MineStoreVersion sinceVersion) {
-        super("This method is supported since version " + sinceVersion.toString() + ", but the current version is " + currentVersion.toString());
-    }
-}
