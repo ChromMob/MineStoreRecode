65676f477f0c5062d14ef8d9cb1d3187346c54d3
chrommob <filip.chromy04@gmail.com>
2025-05-25 12:53:04 +0200

    Add overall player count

--- common/src/main/java/me/chrommob/minestore/common/stats/StatSender.java
+++ common/src/main/java/me/chrommob/minestore/common/stats/StatSender.java
@@ -2,13 +2,16 @@ package me.chrommob.minestore.common.stats;
 
 
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import me.chrommob.minestore.api.Registries;
 import me.chrommob.minestore.api.generic.MineStoreVersion;
 import me.chrommob.minestore.api.stats.BuildConstats;
 import me.chrommob.minestore.common.MineStoreCommon;
 
 import javax.net.ssl.HttpsURLConnection;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -17,6 +20,7 @@ import java.util.UUID;
 public class StatSender {
     private final MineStoreCommon common;
     private final UUID SERVERUUID;
+    private final UUID STORE_UUID;
     private final String JAVA_VERSION;
     private final String PLATFORM_TYPE;
     private final String PLATFORM_NAME;
@@ -30,6 +34,7 @@ public class StatSender {
     public StatSender(MineStoreCommon common) {
         this.common = common;
         SERVERUUID = generateUUIDFromStrings(common, common.pluginConfig().getKey("store-url").getAsString(), common.pluginConfig().getKey("api").getKey("key").getAsString(), common.pluginConfig().getKey("weblistener").getKey("secret-key").getAsString());
+        STORE_UUID = generateUUIDFromStrings(common, common.pluginConfig().getKey("store-url").getAsString(), common.pluginConfig().getKey("api").getKey("key").getAsString());
         JAVA_VERSION = System.getProperty("java.version");
         PLATFORM_TYPE = Registries.PLATFORM.get();
         PLATFORM_NAME = Registries.PLATFORM_NAME.get();
@@ -74,26 +79,12 @@ public class StatSender {
                 int playerCount = Registries.USER_GETTER.get().getAllPlayers().size();
                 StatJson statJson = new StatJson(SERVERUUID, JAVA_VERSION, PLATFORM_TYPE, PLATFORM_NAME, PLATFORM_VERSION, PLUGIN_VERSION, CORE_COUNT, SYSTEM_ARCHITECTURE, MineStoreCommon.version() == MineStoreVersion.dummy() ? "Pre 3.0.0" : MineStoreCommon.version().toString());
                 statJson.setPlayerCount(playerCount);
+                int storePlayerCount = getPlayerCount(common.pluginConfig().getKey("store-url").getAsString());
+                WebStoreJson webStoreJson = new WebStoreJson(STORE_UUID, storePlayerCount);
                 String json = gson.toJson(statJson);
-                common.debug(this.getClass(), "Sending stat json: " + json);
-                HttpsURLConnection connection = null;
-                try {
-                    connection = (HttpsURLConnection) new java.net.URL("https://api.chrommob.fun/minestore/data").openConnection();
-                    connection.setRequestMethod("POST");
-                    connection.setRequestProperty("Content-Type", "application/json");
-                    connection.setDoOutput(true);
-                    connection.setDoInput(true);
-                    connection.getOutputStream().write(json.getBytes());
-                    connection.getOutputStream().flush();
-                    connection.getOutputStream().close();
-                    connection.getInputStream().close();
-                } catch (IOException e) {
-                    common.log(e.getMessage());
-                } finally {
-                    if (connection != null) {
-                        connection.disconnect();
-                    }
-                }
+                sendData(json);
+                json = gson.toJson(webStoreJson);
+                sendStoreData(json);
                 try {
                     Thread.sleep(1000 * 60);
                 } catch (InterruptedException e) {
@@ -102,5 +93,106 @@ public class StatSender {
             }
         });
         thread.start();
+    }
+
+    private void sendData(String json) {
+        common.debug(this.getClass(), "Sending stat json: " + json);
+        HttpsURLConnection connection = null;
+        try {
+            connection = (HttpsURLConnection) new java.net.URL("https://api.chrommob.fun/minestore/data").openConnection();
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/json");
+            connection.setDoOutput(true);
+            connection.setDoInput(true);
+            connection.getOutputStream().write(json.getBytes());
+            connection.getOutputStream().flush();
+            connection.getOutputStream().close();
+            connection.getInputStream().close();
+        } catch (IOException e) {
+            common.log(e.getMessage());
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+        }
+    }
+
+    private void sendStoreData(String json) {
+        common.debug(this.getClass(), "Sending store json: " + json);
+        HttpsURLConnection connection = null;
+        try {
+            connection = (HttpsURLConnection) new java.net.URL("https://api.chrommob.fun/minestore/playerCountData").openConnection();
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/json");
+            connection.setDoOutput(true);
+            connection.setDoInput(true);
+            connection.getOutputStream().write(json.getBytes());
+            connection.getOutputStream().flush();
+            connection.getOutputStream().close();
+            connection.getInputStream().close();
+        } catch (IOException e) {
+            common.log(e.getMessage());
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+        }
+    }
+
+    private int getPlayerCount(String ip, int port) {
+        String url = "https://yamcsrvstatus.chrommob.fun/api/" + ip + ":" + port;
+        HttpsURLConnection connection = null;
+        try {
+            connection = (HttpsURLConnection) new java.net.URL(url).openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("Content-Type", "application/json");
+            connection.setDoOutput(true);
+            connection.setDoInput(true);
+            StringBuilder response = new StringBuilder();
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    response.append(line);
+                }
+            }
+            JsonObject jsonObject = gson.fromJson(response.toString(), JsonObject.class);
+            return jsonObject.getAsJsonObject("players").get("online").getAsInt();
+        } catch (Exception e) {
+            return 0;
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+        }
+    }
+
+    private int getPlayerCount(String storeUrl) {
+        storeUrl = storeUrl.endsWith("/") ? storeUrl : storeUrl + "/";
+        String apiUrl = storeUrl + "api/settings/get";
+        HttpsURLConnection connection = null;
+        try {
+            connection = (HttpsURLConnection) new java.net.URL(apiUrl).openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("Content-Type", "application/json");
+            connection.setDoOutput(true);
+            connection.setDoInput(true);
+            StringBuilder response = new StringBuilder();
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    response.append(line);
+                }
+            }
+            JsonObject jsonObject = gson.fromJson(response.toString(), JsonObject.class);
+            String ip = jsonObject.getAsJsonObject("server").get("ip").getAsString();
+            int port = jsonObject.getAsJsonObject("server").get("port").getAsInt();
+            return getPlayerCount(ip, port);
+        } catch (Exception e) {
+            return 0;
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+        }
     }
 }
