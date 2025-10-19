0a8051356baf149ded9e0fc68cc66a2ac054752b
ChromMob <filip.chromy04@gmail.com>
2025-10-19 16:58:40 +0200

    Implement plugin API to mark payment as paid

--- api/src/main/java/me/chrommob/minestore/api/web/WebApiAccessor.java
+++ api/src/main/java/me/chrommob/minestore/api/web/WebApiAccessor.java
@@ -2,6 +2,7 @@ package me.chrommob.minestore.api.web;
 
 import me.chrommob.minestore.api.web.coupon.CouponManager;
 import me.chrommob.minestore.api.web.giftcard.GiftCardManager;
+import me.chrommob.minestore.api.web.payment.PaymentManager;
 import me.chrommob.minestore.api.web.profile.ProfileManager;
 
 import java.util.function.Function;
@@ -12,6 +13,7 @@ public class WebApiAccessor {
     private static final GiftCardManager giftCardManager = new GiftCardManager(requestHandler);
     private static final ProfileManager profileManager = new ProfileManager(requestHandler);
     private static final CouponManager couponManager = new CouponManager(requestHandler);
+    private static final PaymentManager paymentManager = new PaymentManager(requestHandler);
 
     public static GiftCardManager giftCardManager() {
         return giftCardManager;
@@ -23,6 +25,10 @@ public class WebApiAccessor {
 
     public static CouponManager couponManager() {
         return couponManager;
+    }
+
+    public static PaymentManager paymentManager() {
+        return paymentManager;
     }
 
     public static void registerRequestHandler(Function<WebApiRequest<?>, Result<?, ? extends Exception>> function) {
---
+++ api/src/main/java/me/chrommob/minestore/api/web/payment/PaymentManager.java
@@ -0,0 +1,23 @@
+package me.chrommob.minestore.api.web.payment;
+
+import me.chrommob.minestore.api.generic.ParamBuilder;
+import me.chrommob.minestore.api.web.FeatureManager;
+import me.chrommob.minestore.api.web.Result;
+import me.chrommob.minestore.api.web.WebApiRequest;
+import me.chrommob.minestore.api.web.Wrapper;
+
+import java.util.function.Function;
+
+public class PaymentManager extends FeatureManager {
+    public PaymentManager(Wrapper<Function<WebApiRequest<?>, Result<?, ? extends Exception>>> requestHandler) {
+        super(requestHandler);
+    }
+
+    public boolean markPaymentAsPaid(String paymentId, boolean executeCommands, String note) {
+        ParamBuilder paramBuilder = new ParamBuilder();
+        paramBuilder.append("execute_commands", String.valueOf(executeCommands));
+        paramBuilder.append("note", note);
+        Result<Void, Exception> result = request(new WebApiRequest<>("payment/markAsPaid/" + paymentId, WebApiRequest.Type.POST, paramBuilder, Void.class));
+        return result.error() == null;
+    }
+}
